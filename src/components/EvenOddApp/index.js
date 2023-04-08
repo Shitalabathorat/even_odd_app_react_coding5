f75104d529b786bb5e1bf4718e9337c0ce9a6133
Shitalabathorat <126850348+Shitalabathorat@users.noreply.github.com>
2023-04-08 14:54:38 +0530

    Update index.js

--- src/components/EvenOddApp/index.js
+++ src/components/EvenOddApp/index.js
@@ -1,23 +1,28 @@
 import {Component} from 'react'
 import './index.css'
 
-class EvenOddApp extends Component{
-    state={count : 0,
-           isOdd: true}
-    onIncrementButton=()=>{
-        this.setState( prevState=> ({ count : prevState.count + Math.ceil(Math.random() * 100),
-            isOdd: !prevState.isOdd }))
-    }
-    render(){
-        const {count, isOdd} =this.state
-        return{
-            <div className="component">
-                <h1 className="heading">Count {count}</h1>
-                <h1 className="heading">Count is {isOdd? 'Odd': 'Even'}</h1>
-                <button className="btn" type="button" onClick={this.onIncrementButton}>
-                Increment</button>
-                <p>*Increase by random number from 0 to 100</p>
-            </div>
-        }}
+class EvenOddApp extends Component {
+  state = {count: 0, isEven: true}
+
+  onIncrementButton = () => {
+    this.setState(prevState => ({
+      count: prevState.count + Math.ceil(Math.random() * 100),
+      isEven: !prevState.isEven,
+    }))
+  }
+
+  render() {
+    const {count, isEven} = this.state
+    return (
+      <div className="component">
+        <h1 className="heading">Count {count}</h1>
+        <p className="heading">Count is {isEven ? 'Even' : 'Odd'}</p>
+        <button className="btn" type="button" onClick={this.onIncrementButton}>
+          Increment
+        </button>
+        <p>Increase By Random Number Between 0 to 100</p>
+      </div>
+    )
+  }
 }
 export default EvenOddApp
